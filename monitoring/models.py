a00e8a2d37675fd51b170bf05a3f1f5f91d1eeae
Murat Kamaletdinov <kamaletdinov2425@icloud.com>
2020-12-02 23:44:51 +0300

    Code review from Artur Atnagulov, added new TODO's last_check_time now DateTimeField in database (replaced from CharField)

--- monitoring/models.py
+++ monitoring/models.py
@@ -3,7 +3,8 @@ from django.db import models
 
 class Url(models.Model):
     url = models.CharField(max_length=100)
-    last_check_time = models.CharField(max_length=25)
+    last_check_time = models.DateTimeField()
+    # TODO DateTimeField
     status_code = models.IntegerField()
     status = models.CharField(max_length=10, null=True)
     error = models.CharField(max_length=100)
