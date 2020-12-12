585ebf0ef175a22bdac5a81a1cef4a9fb2528d25
Murat Kamaletdinov <kamaletdinov2425@icloud.com>
2020-12-12 10:42:18 +0300

    change Url model fields url and final url from CharField to URLField

--- monitoring/models.py
+++ monitoring/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 
 class Url(models.Model):
-    url = models.CharField(max_length=100)
+    url = models.URLField(null=False)
     last_check_time = models.DateTimeField(auto_now=True)
     status_code = models.IntegerField(null=True)
     status = models.CharField(max_length=10, null=True)
     error = models.CharField(max_length=100, null=True)
-    final_url = models.CharField(max_length=100, null=True)
+    final_url = models.URLField(null=True)
 
     def __str__(self):
         return self.url
